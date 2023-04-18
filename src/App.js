af0335c4e48b6b1be4e3befcd7d2f588960a13dc
KazakhBas1k <darhan.balmukhambet@gmail.com>
2023-04-18 17:53:13 +0600

    feat: added layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [active, setActive] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="container">
+        <h1>To Do List</h1>
+        <div className="todo__container">
+          <div className="item">
+            <div className="item__info">
+              <p>Buy groceries </p>
+              <span>High</span>
+            </div>
+            <div className="item__actions">
+              <button>
+                <img src="./assets/edit.svg" alt="edit__btn" />
+              </button>
+              <button>
+                <img src="./assets/delete.svg" alt="delete__btn" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <button
+          className="add__btn"
+          onClick={() => {
+            setActive(true);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <img src="./assets/add.svg" alt="add__btn" />
+        </button>
+      </div>
+      {active ? (
+        <div className="popup__wrapper">
+          <div className="popup__container">
+            <div className="popup__top">
+              <h2>ToDo</h2>
+              <button onClick={() => {
+                setActive(false)
+              }}>
+                <img src="./assets/close.svg" alt="close__btn" />
+              </button>
+            </div>
+            <div className="popup__form">
+              <input type="text" placeholder="Title" />
+              <select required>
+                <option value="" key="1" selected disabled>
+                  Category
+                </option>
+                <option value="high" key="2">
+                  High
+                </option>
+                <option value="medium" key="3">
+                  Medium
+                </option>
+                <option value="low" key="4">
+                  Low
+                </option>
+              </select>
+              <button>Create</button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
